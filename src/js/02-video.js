d3edc534420efc8afa1bafc8cf00161d2034d276
KovalenkoSerhiy <118220875+KovalenkoSerhiy@users.noreply.github.com>
2023-07-17 23:04:51 +0300

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,34 +1,26 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const STORAGE_KEY = 'videoplayer-current-time';
+const player = new Player('vimeo-player');
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
-const iframe = document.querySelector('iframe');
-const player = 
+player.on(
+  'timeupdate',
+  throttle(data => {
+    const LOCALSTORAGE_VALUE = data.seconds;
+    localStorage.setItem(LOCALSTORAGE_KEY, LOCALSTORAGE_VALUE);
+  }, 2000)
+);
 
-
-
-
-// const player = new Player('vimeo-player');
-// const LOCALSTORAGE_KEY = 'videoplayer-current-time';
-
-// player.on(
-//   'timeupdate',
-//   throttle(data => {
-//     const LOCALSTORAGE_VALUE = data.seconds;
-//     localStorage.setItem(LOCALSTORAGE_KEY, LOCALSTORAGE_VALUE);
-//   }, 2000)
-// );
-
-// player
-//   .setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY))
-//   .then(function () {})
-//   .catch(function (error) {
-//     switch (error.name) {
-//       case 'RangeError':
-//         break;
-//       default:
-//         break;
-//     }
-//   });
+player
+  .setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY))
+  .then(function () {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
 
