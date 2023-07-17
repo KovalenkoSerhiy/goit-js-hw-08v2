25abf09bf5b45137804f6e32bb2c9c32d93e5671
KovalenkoSerhiy <118220875+KovalenkoSerhiy@users.noreply.github.com>
2023-07-17 16:10:52 +0300

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,37 +1,23 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const player = new Player('vimeo-player');
-const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const STORAGE_KEY = 'videoplayer-current-time';
 
-player.on(
-  'timeupdate',
-  throttle(data => {
-    const LOCALSTORAGE_VALUE = data.seconds;
-    localStorage.setItem(LOCALSTORAGE_KEY, LOCALSTORAGE_VALUE);
-  }, 2000)
-);
+const iframe = document.querySelector('iframe');
+const player = 
 
-player
-  .setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY))
-  .then(function () {})
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        break;
-      default:
-        break;
-    }
-  });
 
-// варіант 2
-// function updatePosition(seconds) {
-//   localStorage.setItem(LOCALSTORAGE_KEY, seconds);
-// }
+
+
+// const player = new Player('vimeo-player');
+// const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
 // player.on(
 //   'timeupdate',
-//   throttle(data => updatePosition(data.seconds), 1000)
+//   throttle(data => {
+//     const LOCALSTORAGE_VALUE = data.seconds;
+//     localStorage.setItem(LOCALSTORAGE_KEY, LOCALSTORAGE_VALUE);
+//   }, 2000)
 // );
 
 // player
@@ -46,25 +32,3 @@ player
 //     }
 //   });
 
-// варіант 1
-//   function updatePosition(seconds) {
-//     localStorage.setItem(LOCALSTORAGE_KEY, seconds);
-//   }
-
-//   const throttledUpdatePosition = throttle(updatePosition, 1000);
-
-//   player.on('timeupdate', function (data) {
-//     throttledUpdatePosition(data.seconds);
-//   });
-
-//   player
-//     .setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY))
-//     .then(function () {})
-//     .catch(function (error) {
-//       switch (error.name) {
-//         case 'RangeError':
-//           break;
-//         default:
-//           break;
-//       }
-//     });
